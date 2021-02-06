e2b2e7746655d009136c09a3bf73a0048f0df82c
ermz <eramirez4369@gmail.com>
2021-02-06 03:24:49 -0500

    Finally fixed tests and learned how to use chai and cha setup

--- test/escrow.js
+++ test/escrow.js
@@ -1,49 +1,56 @@
 const Escrow = artifacts.require("Escrow");
-const expect = require('chai').expect;
 
-const assertError = async (promise, e) => {
-	try {
-		await promise;
-	} catch(e) {
-		assert(e.message.includes(error));
-		return;
-	}
-	assert(false);
-}
+//chai setup
+const chai = require('chai');
+const expect = chai.expect;
+const BN = web3.utils.BN;
+const chaiBN = require('chai-BN')(BN);
+chai.use(chaiBN);
+
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
 
 contract("Escrow", async accounts => {
-	const [lawyer, payer, recipient] = accounts;
-	let escrow = null;
+	let [lawyer, payer, recipient] = accounts;
+	let escrow = null
 
 	before(async () => {
 		escrow = await Escrow.deployed();
 	});
 
-	it("should allow you to deposit", async () => {
-		// let escrow = await Escrow.deployed();
+	it("should allow deposits to be made", async() => {
 		await escrow.deposit({from: payer, value: 900});
-		const escrowBalance = parseInt(await web3.eth.getBalance(escrow.address));
-		assert.equal(escrowBalance, 900);
+		let escrowBalance = parseInt(await web3.eth.getBalance(escrow.address));
+		return expect(escrowBalance).to.equal(900);
 	});
 
-	it("should NOT deposit if it exceeds amount necessary", () => {
-		assertError(
-			escrow.deposit({from: payer, value: 101}),
-			"Will overflow amount limit"
-		);
+	it("should allow me to add two number", async () => {
+		return expect(1).to.equal(1);
 	});
 
-	// it("should not deposit if not sending from payer", async () => {
-	// 	assertError(
-	// 		escrow.deposit({from: accounts[6]}),
-	// 		"Only payer is allowed to deposit"
-	// 	);
+
+	// it("should NOT allow a deposit by anyone othen that the payer", async() => {
+	// 	expect(await escrow.deposit({from: accounts[4], value: 50})).to.throw('Only payer is allowed to deposit');
 	// });
 
-	it("Should allow release if lawyer and exceeds amount", async () => {
-		await escrow.deposit({from: payer, value: 100});
-		let newBalance = parseInt(await web3.getBalance(escrow.address));
-		assert(newBalance == 0);
-	})	 
+	it("should NOT allow deposits from anyone other that the payer", async () => {
+		return expect(escrow.deposit({from: lawyer, value: 50})).to.be.rejected;
+	});
+
+	it("should NOT allow to deposit if it will go over the amount limit", async () => {
+		//Limit was 1000
+		return expect(escrow.deposit({from: payer, value: 1001})).to.be.rejected;
+	});
+
+	it("should NOT release before the proper amount is ", async () => {
+		await escrow.deposit({from: payer, value: 99});
+		//makes sure that you need 100 to actually use release function
+		await expect(escrow.release({from: lawyer})).to.be.rejected;
+		await escrow.deposit({from: payer, value: 1});
+		//makes sure that only lawyer is able to use release function
+		await expect(escrow.release({from: recipient})).to.be.rejected;
+		//Makes sure that release works when all requirements are met
+		return expect(escrow.release({from: lawyer})).to.not.be.rejected;
+	})
 
 });
